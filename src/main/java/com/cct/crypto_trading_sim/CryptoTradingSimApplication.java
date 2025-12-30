40acc311778098442c2fe5ba9832ca92f90f3773
syednuman <syed.numan@clayfin.com>
2025-12-30 17:42:41 +0530

    made project Build ready

--- src/main/java/com/cct/crypto_trading_sim/CryptoTradingSimApplication.java
+++ src/main/java/com/cct/crypto_trading_sim/CryptoTradingSimApplication.java
@@ -2,13 +2,20 @@ package com.cct.crypto_trading_sim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
-public class CryptoTradingSimApplication {
+public class CryptoTradingSimApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CryptoTradingSimApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoTradingSimApplication.class, args);
